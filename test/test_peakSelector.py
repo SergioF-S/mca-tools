733302dcb91fe5e8d18cafd356ae4a008933526f
Diego Couto García <63312571+dcoutogarcia@users.noreply.github.com>
2026-04-09 08:36:57 +0000

    Modified test file

--- test/test_peakSelector.py
+++ test/test_peakSelector.py
@@ -3,7 +3,7 @@ import mca_tools as mca
 mca.select_language("gl")
 
 Bi = mca.peakSelector("test/Bi.mca", bkg_file = "test/Background.mca", peak_energies = [569.69, 1063.656], fig_path = "test/output/fig", csv_path = "test/output/csv")
-Co = mca.peakSelector("test/Co.mca", bkg_file= "test/Background.mca", bins_fused = 20, peak_energies = [1173.228, 1332.492], fig_path = "test/output/fig", csv_path = "test/output/csv")
+Co = mca.peakSelector("test/Co.mca", bkg_file= "test/Background.mca", peak_energies = [1173.228, 1332.492], fig_path = "test/output/fig", csv_path = "test/output/csv")
 Eu = mca.peakSelector("test/Eu.mca", bkg_file = "test/Background.mca", fig_path = "test/output/fig", csv_path = "test/output/csv")
 
 Bi.select_peaks()
